dd1f5c2dabe19fa382ff118b9f4c06b2085c765a
yanglet <y2yone023@naver.com>
2022-07-02 18:44:44 +0900

    feat: LocalDateTime 데이터 포맷 변경

--- src/main/java/com/report/triple/domain/point/response/PointLogDto.java
+++ src/main/java/com/report/triple/domain/point/response/PointLogDto.java
@@ -1,8 +1,11 @@
 package com.report.triple.domain.point.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.report.triple.domain.point.entity.PointLog;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -11,11 +14,14 @@ public class PointLogDto {
     private String action;
     private int point;
     private String reviewId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createTime;
 
     public PointLogDto(PointLog pointLog) {
         this.userId = pointLog.getUserId();
         this.action = pointLog.getAction();
         this.point = pointLog.getPoint();
         this.reviewId = pointLog.getReviewId();
+        this.createTime = pointLog.getCreateTime();
     }
 }
