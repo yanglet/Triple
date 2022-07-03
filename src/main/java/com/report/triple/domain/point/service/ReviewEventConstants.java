755d3dceaa7159983f61bb370bead87c8d7f50db
yanglet <y2yone023@naver.com>
2022-07-03 23:54:05 +0900

    refactor: 기타 수정 및 변경

--- src/main/java/com/report/triple/domain/point/service/ReviewEventConstants.java
+++ src/main/java/com/report/triple/domain/point/service/ReviewEventConstants.java
@@ -1,5 +1,9 @@
 package com.report.triple.domain.point.service;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReviewEventConstants {
     public static final String REVIEW_EVENT_TYPE = "REVIEW";
     public static final String REVIEW_EVENT_ACTION_ADD = "ADD";
